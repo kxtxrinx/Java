55a27c634dafef5261ef5474dda71bb4e02c991c
Aya <sioqueras@gmail.com>
2023-04-10 10:42:11 +0200

    Optimized version!
    - Optimized generate() function
    - Put arrays as PassGen attributes
    - Added the option to choose the pass length

--- PassGen.java
+++ PassGen.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
 public class PassGen {
 
-    public static String generate(){
-        String[] aWords = {"cat","pet","sun","rise"};
-        char[] aChar = {'z','y','x','j','w'};
-        int[] aNumber = {1,2,3,4,5,6,7,8,9};
+    private static String[] aWords = {"cat","pet","sun","rise","sus","peep","uh","btw","less"};
+    private static char[] aChar = {'z','y','x','j','w','X','Z','W','J','F'};
+    private static int[] aNumber = {1,2,3,4,5,6,7,8,9};
 
-        //will be optimized soon!!
-        String password = "" + aNumber[getRandomNumber(aNumber)] + aChar[getRandomChar(aChar)] + aChar[getRandomChar(aChar)] + aNumber[getRandomNumber(aNumber)] + aWords[getRandomWord(aWords)] + aChar[getRandomChar(aChar)] + aNumber[getRandomNumber(aNumber)] + aNumber[getRandomNumber(aNumber)] + aNumber[getRandomNumber(aNumber)] + aNumber[getRandomNumber(aNumber)];
+    //more optimized version!! can be better tho :)
+    public static String generate(int passLength){
+        StringBuilder sb = new StringBuilder(); // sb == password
 
-        return password;
+        for(int i=0;i<passLength;i++){
+            int opt = (int)(Math.random()*3);
+            if (opt == 0){
+                sb.append(aWords[(int)(Math.random()*aWords.length)]);
+            }else if (opt == 1){
+                sb.append(aChar[(int)(Math.random()*aChar.length)]);
+            }else{
+                sb.append(aNumber[(int)(Math.random()*aNumber.length)]);
+            }
+        }
+        return sb.toString();
     }
 
+    /*
     public static int getRandomWord(String[] aWords){
         int posW = (int)(Math.random()*aWords.length);
         return posW;
@@ -25,12 +37,32 @@ public class PassGen {
         int posN = (int)(Math.random()*aNumber.length);
         return posN;
     }
+     */
 
 
     public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
 
-        System.out.println(PassGen.generate());
+        int passLength = 0;
+        do{
+            System.out.println("How long do you want your pass to be?");
+            passLength = s.nextInt();
+            if(passLength<0){
+                System.out.println("Please, enter an integer number greater than 0");
+            }
+        }while(passLength<1);
 
+        System.out.println(PassGen.generate(passLength)); //pass generation
+
+        if(passLength<5){
+            System.out.println("Uh...it's kinda weak, be carefull out there");
+        }else if(passLength>=5 && passLength<=8){
+            System.out.println("Not that bad, but could be better.");
+        }else{
+            System.out.println("Yeah that's definitely a pretty strong pass!! Don't leak it :)");
+        }
+
+        s.close();
 
     }
 }
